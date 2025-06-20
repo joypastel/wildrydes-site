6e6501c1ca08d3c049f39768ce0e4e3405233745
joypastel <eunsoo@gmail.com>
2025-06-19 23:07:37 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://cognito-idp.us-west-2.amazonaws.com/us-west-2_H5MtKCzi0' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
