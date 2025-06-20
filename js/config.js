869889d46768c3757d54a4662e46d18a38a21894
joypastel <eunsoo@gmail.com>
2025-06-19 23:33:29 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_H5MtKCzi0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '392m3q7mvnrbpli6639af5e09c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_0zRf91kLh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3casn9kd8kqlgcu63tinb99oli', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
